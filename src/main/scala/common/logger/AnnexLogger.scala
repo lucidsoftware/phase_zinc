aea9bb216be65de6e8c2dd025baa148dc7f63684
Bor Kae Hwang <borkaehw@lucidchart.com>
2020-01-31 15:45:17 -0700

    Add workers

---
+++ src/main/scala/common/logger/AnnexLogger.scala
@@ -0,0 +1,45 @@
+package phase_zinc.common.logger
+
+import xsbti.Logger
+
+import java.io.{PrintStream, PrintWriter, StringWriter}
+import java.nio.file.Paths
+import java.util.function.Supplier
+
+import CommonArguments.LogLevel
+
+final class AnnexLogger(level: String, out: PrintStream = System.err) extends Logger {
+
+  private[this] val root = s"${Paths.get("").toAbsolutePath}/"
+
+  private[this] def format(value: String): String = value.replace(root, "")
+
+  def debug(msg: Supplier[String]): Unit = level match {
+    case LogLevel.Debug => out.println(format(msg.get))
+    case _              =>
+  }
+
+  def error(msg: Supplier[String]): Unit = level match {
+    case LogLevel.Debug | LogLevel.Error | LogLevel.Info | LogLevel.Warn => out.println(format(msg.get))
+    case _                                                               =>
+  }
+
+  def info(msg: Supplier[String]): Unit = level match {
+    case LogLevel.Debug | LogLevel.Info => out.println(format(msg.get))
+    case _                              =>
+  }
+
+  def trace(err: Supplier[Throwable]): Unit = level match {
+    case LogLevel.Debug | LogLevel.Error | LogLevel.Info | LogLevel.Warn =>
+      val trace = new StringWriter();
+      err.get.printStackTrace(new PrintWriter(trace));
+      out.println(format(trace.toString))
+    case _ =>
+  }
+
+  def warn(msg: Supplier[String]): Unit = level match {
+    case LogLevel.Debug | LogLevel.Info | LogLevel.Warn => out.println(format(msg.get))
+    case _                                              =>
+  }
+
+}
---
+++ src/main/scala/common/logger/AnnexScalaInstance.scala
@@ -0,0 +1,30 @@
+package phase_zinc.common.logger
+
+import xsbti.compile.ScalaInstance
+
+import java.io.File
+import java.net.URLClassLoader
+import java.util.Properties
+
+final class AnnexScalaInstance(val allJars: Array[File]) extends ScalaInstance {
+
+  override def version: String = actualVersion
+  override lazy val actualVersion: String = {
+    val stream = loader.getResourceAsStream("compiler.properties")
+    try {
+      val props = new Properties
+      props.load(stream)
+      props.getProperty("version.number")
+    } finally stream.close()
+  }
+
+  override def compilerJar: File = null
+  override lazy val libraryJar: File = allJars
+    .find(f => new URLClassLoader(Array(f.toURI.toURL)).findResource("library.properties") != null)
+    .get
+
+  override def otherJars: Array[File] = null
+  override lazy val loader: ClassLoader =
+    new URLClassLoader(allJars.map(_.toURI.toURL), null)
+  override def loaderLibraryOnly: ClassLoader = null
+}
